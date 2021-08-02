93b7c19a5066c14019d95d4c528b4278fef670d7
maceliem <mattias@kirkaune.com>
2021-08-02 08:31:00 +0200

    Added scale to level cam

--- Scripts/UI/ResourceIndicator.gd
+++ Scripts/UI/ResourceIndicator.gd
@@ -1,14 +1,14 @@
 extends Control
-
+export var scale:Vector2 = Vector2(2,2)
 func _ready():
 	$VBoxContainer/HBoxContainer.rect_size.x = str(Global.currencies["coins"]).length()*16 +68
 	self.anchor_left = 1 - $VBoxContainer/HBoxContainer.rect_size.x/1024
 	self.anchor_top =  0.01
 	if self.get_parent().get_name() =="Camera2D": #if in game
-		self.rect_scale = Vector2(2,2)
+		self.rect_scale = scale
 		self.rect_position = Vector2(
-				1024 - $VBoxContainer/HBoxContainer.rect_size.x*2,
-				-600 + 0.01
+				512*scale.x - $VBoxContainer/HBoxContainer.rect_size.x*scale.x,
+				-(300*scale.y) + 0.01
 			)
 
 func _process(_delta): #indicate how many coins we have
