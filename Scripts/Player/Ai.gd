03cad7532302977e90e2d4a66dc003abfeca0421
nicklasbns (laptop) <nicklasbns@gmail.com>
2021-08-16 16:09:17 +0200

    progress

--- Scripts/Player/Ai.gd
+++ Scripts/Player/Ai.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-
-var level #to be used                    ----
+var main
+var level
 var snake #the snake object
-var main #to be used                     ----
-var fruits = []
+var fruits
 var map = [] #the map i made
 var mapPos = [] #the map mattias gives me
+var pos = [-1, 2] #maptileindex, rotation
 var time = 0 #throwaway time variable
 var reset = [0, false] #when to stop pressing
 var dir = ["Up", "Right", "Down", "Left"]
@@ -15,26 +15,31 @@ var action = 0 #what way do we turn?
 var temp = 0
 
 func _ready():
-	print(len(get_parent().get_parent().get_child(0).get_child(0).get_children()))
-	snake = get_parent().get_parent().get_child(0).get_child(0)
-	mapPos = get_parent().get_parent().get_parent().mapPos
+	main = self.get_parent().get_parent().get_parent().get_parent()
+	level = main.get_child(4)
+	snake = self.get_parent().get_child(0)
+	fruits = level.get_child(3)
+	print(fruits)
+	mapPos = level.mapPos
 	get_parent().get_parent().get_parent().get_child(6).modulate = Color(1, 1, 1, 0.3)
-	get_parent().get_parent().get_child(1).connect("timeout", self, "readyDraw");
+	get_parent().get_parent().get_child(1).connect("timeout", self, "step");
 
-	for pos in mapPos:
-		map.push_back([pos, -1, -1, -1, -1])
+	for elem in mapPos:
+		map.push_back([elem, -1, -1, -1, -1])
+		if map[-1][0] == snake.get_child(1).position:
+			pos = [len(map), 2]
 	for i in len(map):
 		for j in len(map):
-			if map[i][0].y == map[j][0].y:
-				if map[i][0].x-64 == map[j][0].x:
-					map[i][1] = j
-				if map[i][0].x+64 == map[j][0].x:
-					map[i][3] = j
 			if map[i][0].x == map[j][0].x:
 				if map[i][0].y-64 == map[j][0].y:
-					map[i][2] = j
+					map[i][1] = j
 				if map[i][0].y+64 == map[j][0].y:
+					map[i][3] = j
+			if map[i][0].y == map[j][0].y:
+				if map[i][0].x-64 == map[j][0].x:
 					map[i][4] = j
+				if map[i][0].x+64 == map[j][0].x:
+					map[i][2] = j
 	#Input.action_press("Left")
 			
 
@@ -44,16 +49,27 @@ func _process(_delta):
 	time += _delta
 	if reset[0] and reset[0]-time < -0.3:
 		Input.action_release(reset[1])
-		print(reset[1])
+		#print(reset[1])
 		reset = [0, false]
 	pass
 
-func readyDraw():
-	temp += 1
-	action = round(snake.get_child(0).rotation*2/PI)+2
-	if temp == 3:
-		go(action-1)
-		temp = 0
+func step(): # the main calculator
+	var rotation = round(snake.get_child(0).rotation*2/PI)+2
+	print(rotation)
+	pos = [map[pos[0]][rotation+1], rotation]
+	var diff = Vector2(0, 0)
+	diff = fruits.get_children()[0].position-map[pos[0]][0]
+	print(diff)
+	if abs(diff.x) > abs(diff.y):
+		if diff.x > 0:
+			go(1)
+		else:
+			go(3)
+	else:
+		if diff.y > 0:
+			go(2)
+		else:
+			go(0)
 	self.update()
 
 func _draw():
@@ -67,10 +83,12 @@ func _draw():
 				self.draw_line(i[0], map[i[j+1]][0], Color(0.2, 0.7, 0.2))
 	for part in snake.get_children():
 		self.draw_circle(part.position, 10, Color(1, 0.4, 0.4))
+	self.draw_rect(Rect2(map[pos[0]][0]+Vector2(-15, -15), Vector2(30, 30)), Color(0xff), true)
 	self.draw_line(Vector2(-500, -500), Vector2(-500, -500)+Vector2(50, 0).rotated(action*PI), Color(0xff), 2)
 	pass
 
 
 func go(act:int):
+	print(act)
 	Input.action_press(dir[act])
 	reset = [time, dir[act]]
